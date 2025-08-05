10b1ee0fa0ef34ea1b857bc45963f306f17286ec
Souzxp <luizbueno097@gmail.com>
2025-08-05 14:52:30 -0300

    5/8

---
+++ estudorotas/src/Paginas/Home.jsx
@@ -0,0 +1,11 @@
+export default function Home()
+{
+    return (
+        <div>
+            <h1>Estudo de Rotas</h1>
+            <ul>
+                <li><a href="/sp">Sao Paulo</a></li>
+            </ul>
+        </div>
+    )
+}
---
+++ estudorotas/src/Paginas/SaoPaulo.jsx
@@ -0,0 +1,19 @@
+export default function SaoPaulo()
+{
+    return(
+        <div>
+            <h1>Seja bem vindo ao Estado de São Paulo</h1>
+            <p>São Paulo é o estado mais rico e populoso do Brasil, localizado na região Sudeste. É o principal centro financeiro, industrial e cultural do país, com a capital homônima sendo uma das maiores metrópoles do mundo. O estado possui uma economia diversificada, com destaque para agricultura, indústria, comércio e serviços. São Paulo também é conhecido pela sua diversidade cultural e infraestrutura avançada.
+</p>
+<p>
+
+
+<img src="" />
+</p>
+
+<p>
+    <a href="/">Voltar para Home</a>
+</p>
+            </div>
+    )
+}
