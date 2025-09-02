cb9dc750eeaeb3e597b363abe78455e8e6f4dafa
Souzxp <luizbueno097@gmail.com>
2025-09-02 14:54:08 -0300

    02/09

---
+++ estudoestados/src/Paginas/Exemplo2.jsx
@@ -0,0 +1,47 @@
+export default function Exemplo2() {
+  return (
+    <div>
+      <h1>Exemplo 2</h1>
+
+      <div className="conteudo">
+
+        <h3>Calculo da Média Aritmética</h3>
+
+        <p>
+          O objetivo será o nome do aluno, suas 3 notas e calcular a média.
+
+        </p>
+
+        <form>
+<p>
+  Nome do Aluno: <br />
+  <input type="text" />
+</p>
+
+<p>
+  Nota 1: <br />
+  <input type="text" />
+</p>
+
+<p>
+  Nota 2: <br />
+  <input type="text" />
+</p>
+
+<p>
+
+    <input type="button" value="Calcular" />
+
+  </p>
+
+</form>
+
+      </div>
+
+
+
+
+      
+    </div>
+  );
+}
---
+++ estudoestados/src/Paginas/Home.jsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Estudo de Estados</h1>
+
+      <div className="conteudo">
+        <p>
+          O useState é um hook do React que permite adicionar e gerenciar estados em componentes funcionais. Com ele, você pode criar variáveis que armazenam valores dinâmicos, como textos, números ou objetos, e atualizar esses valores conforme o usuário interage com a interface. Sempre que o estado muda, o componente é re-renderizado automaticamente para refletir as novas informações na tela.
+        </p>
+
+        <ul>
+            <li><Link to="/exemplo1">Exemplo 1</Link></li>
+            <li><Link to="/exemplo2">Exemplo 2</Link></li>
+            
+        </ul>
+
+<h3>Exercícios</h3>
+
+<p>Na aula do dia 09/09/2025 iremos fazer os 4 exercicios abaixo:</p>
+
+<li><Link to="/exercicio1">Exercicio 1</Link></li>
+            <li><Link to="/exercicio2">Exercicio 2</Link></li>
+            <li><Link to="/exercicio3">Exercicio 3</Link></li>
+            <li><Link to="/exercicio4">Exercicio 4</Link></li>
+
+      </div>
+    </div>
+  );
+}
