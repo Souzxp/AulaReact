529598364eeadb4382fb2e4cb44dd6377dafa04c
Souzxp <luizbueno097@gmail.com>
2025-09-23 14:48:55 -0300

    23/09

--- estudoestados/src/Paginas/Exemplo2.jsx
+++ estudoestados/src/Paginas/Exemplo2.jsx
@@ -1,4 +1,25 @@
+import { useState } from "react";
+
 export default function Exemplo2() {
+
+const[numero1, setNumero1] = useState(10);
+const[numero2, setNumero2] = useState(20);
+const [resultado, setResultado] = useState();
+
+function somar ()
+
+{
+
+let n1, n2, n3, resultado;
+n1= Number(numero1);
+n2= Number(numero2);
+n3 =Number(numero3);
+somar= (n1 + n2 + n3) /2;
+
+setResultado("A media dos números é " + media);
+
+}
+
   return (
     <div>
       <h1>Exemplo 2</h1>
@@ -20,12 +41,21 @@ export default function Exemplo2() {
 
 <p>
   Nota 1: <br />
-  <input type="text" />
+  <input type="text" value={numero1}
+  onChange={(e)  => setNumero1(e.target.value) } />
 </p>
 
 <p>
   Nota 2: <br />
-  <input type="text" />
+  
+  <input type="text" value={numero2}
+  onChange={(e)  => setNumero2(e.target.value) } />
+</p>
+
+<p>
+  Nota 3: <br />
+  <input type="text" value={numero3}
+  onChange={(e)  => setNumero3(e.target.value) } />
 </p>
 
 <p>
@@ -33,6 +63,9 @@ export default function Exemplo2() {
     <input type="button" value="Calcular" />
 
   </p>
+  <p>
+  <a href="/">Voltar</a>
+</p>
 
 </form>
 
